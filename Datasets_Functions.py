95b44826b50d05c1b7483287aafe579c5cabf834
JohnPapakostas <84929430+JohnPapakostas@users.noreply.github.com>
2023-08-28 20:30:36 +0300

    Updates in Names and Add update_outer_hashmap
    Updates in names in Datasets_Functions, MLP_Experiments, datasets_hasmap

--- Datasets_Functions.py
+++ Datasets_Functions.py
@@ -1,88 +1,68 @@
 import pickle
 import importlib
 
-def createHashMap(filePath):
-    # Create or load the hashmap from a file
-    filename = filePath+'datasets_hashmap.pkl'
+def create_hashmap(file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
     
     try:
-        # Try to load the existing hashmap from the file
-        with open(filename, 'rb') as file:
+        with open(file_name, 'rb') as file:
             hashmap = pickle.load(file)
     except FileNotFoundError:
-        # If the file doesn't exist, create a new empty hashmap
         hashmap = {}
     
-    # Save the hashmap to the file
-    with open(filename, 'wb') as file:
+    with open(file_name, 'wb') as file:
         pickle.dump(hashmap, file)
         
-def getHashMap(filePath):
-    # Load the hashmap from a file
-    filename = filePath+'datasets_hashmap.pkl'
+def get_hashmap(file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
 
     try:
-        with open(filename, 'rb') as file:
+        with open(file_name, 'rb') as file:
             hashmap = pickle.load(file)
             return hashmap
     except FileNotFoundError:
         print("Hashmap file not found.")      
         return {}
         
-def appendInHashMap(has_key,key,value,filePath):
-    import pickle
-
-    # Load the hashmap from a file
-    filename = filePath+'datasets_hashmap.pkl'
+def append_in_hashmap(has_key, key, value, file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
     
     try:
-        with open(filename, 'rb') as file:
+        with open(file_name, 'rb') as file:
             hashmap = pickle.load(file)
     except FileNotFoundError:
         print("Hashmap file not found.")
         hashmap = {}
     
-    # Append a value to an existing or not key
     hashmap[has_key][key] = value
     
-    # Save the updated hashmap to the file
-    with open(filename, 'wb') as file:
+    with open(file_name, 'wb') as file:
         pickle.dump(hashmap, file)
     
-    print(hashmap)
     return hashmap
-def deleteFromHashMap(key,filePath):
-    import pickle
-
-    # Load the hashmap from a file
-    filename = filePath+'datasets_hashmap.pkl'
+def delete_from_hashmap(key, file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
     
     try:
-        with open(filename, 'rb') as file:
+        with open(file_name, 'rb') as file:
             hashmap = pickle.load(file)
     except FileNotFoundError:
         print("Hashmap file not found.")
         hashmap = {}
     
-    # Append a value to an existing key
     if key in hashmap:
         hashmap.pop(key)
     
-    # Save the updated hashmap to the file
-    with open(filename, 'wb') as file:
+    with open(file_name, 'wb') as file:
         pickle.dump(hashmap, file)
     
-    print(hashmap)
     return hashmap
 
-def updateHashMap(key,value,filePath):
-    import pickle
-
-    # Load the hashmap from a file
-    filename = filePath+'datasets_hashmap.pkl'
+def update_outer_hashmap(key, value, file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
     
     try:
-        with open(filename, 'rb') as file:
+        with open(file_name, 'rb') as file:
             hashmap = pickle.load(file)
     except FileNotFoundError:
         print("Hashmap file not found.")
@@ -90,40 +70,38 @@ def updateHashMap(key,value,filePath):
     
     hashmap[key] = value
     
-    # Save the updated hashmap to the file
-    with open(filename, 'wb') as file:
+    with open(file_name, 'wb') as file:
         pickle.dump(hashmap, file)
-    
-    print(hashmap)
+        
     return hashmap
 
-def makeHashMapWithListsToHashmapWithDicts(filePath,hash_map_with_lists, keys_list):
-    hash_map_with_dicts = {}
-
-    for key, value_list in hash_map_with_lists.items():
-        # Check if the number of keys matches the number of values in the list
-        if len(keys_list) != len(value_list):
-            raise ValueError("Number of keys and values in the list do not match.")
+def update_inner_hashmap(keys, new_value, file_path):
+    file_name = file_path + 'datasets_hashmap.pkl'
+    
+    try:
+        with open(file_name, 'rb') as file:
+            hashmap = pickle.load(file)
+    except FileNotFoundError:
+        print("Hashmap file not found.")
+        hashmap = {}
+    
+    if keys[0] in hashmap:
+        current_dict = hashmap[keys[0]]
+        if keys[1] in current_dict:
+            current_dict[keys[1]] = new_value
+        else:
+            raise KeyError(f"Key '{keys[1]}' not found in dictionary.")
+    else:
+        raise KeyError(f"Key '{keys[0]}' not found in dictionary.")
         
-        # Convert the list to a dictionary using the keys from the keys_list
-        value_dict = {str(keys_list[i]): value_list[i] for i in range(len(value_list))}
-        hash_map_with_dicts[key] = value_dict
-    
-    # Create or load the hashmap from a file
-    filename = filePath+'datasets_hashmap_v2.pkl'
+    with open(file_name, 'wb') as file:
+        pickle.dump(hashmap, file)    
 
-    # Save the hashmap to the file
-    with open(filename, 'wb') as file:
-        pickle.dump(hash_map_with_dicts, file)
-    return hash_map_with_dicts
+def function_get_dataset(dataset_properties):
+    module = importlib.import_module(dataset_properties['module_name'])
+    function = getattr(module, dataset_properties['function_name'])
     
-
-def functionGetDataset(datasetProperties):
-    # Import the module dynamically
-    module = importlib.import_module(datasetProperties['module_name'])
-    # Get the function dynamically
-    function = getattr(module, datasetProperties['function_name'])
-    return function(batch_size=datasetProperties['batch_size'], option_name=datasetProperties['option_name'])
+    return function(batch_size=dataset_properties['batch_size'], option_name=dataset_properties['option_name'])
         
         
         
